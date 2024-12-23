ec6b6d2fb73cf0f6e4fca5d7a0eda7cd26687f02
Mayowa Julius <69092079+Phenzic@users.noreply.github.com>
2024-12-23 19:47:23 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 1894
            Language: Python3
            Runtime: 38 ms
            Runtime percentile: 38.89
            Memory: 17.6 MB
            Memory percentile: 6.81

--- 1894. Merge Strings Alternately/Solution.py
+++ 1894. Merge Strings Alternately/Solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         small = min(len(word1), len(word2))
-        print(small)
         ans = ""
         for i in range(small):
             ans += word1[i]
             ans += word2[i]
-        print(i)
 
         ban = (len(word1)>len(word2))
-        print(ban)
         if ban == True:
             for i in range(small, len(word1)):
                 ans += word1[i]
         elif ban == False:
             for i in range(small, len(word2)):
-                print(i)
                 ans += word2[i]
         return ans
