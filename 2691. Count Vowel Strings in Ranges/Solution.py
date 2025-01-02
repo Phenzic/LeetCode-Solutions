528aa1b4fec4351f88c5a877494d9a34cb99627a
Mayowa Julius <69092079+Phenzic@users.noreply.github.com>
2025-01-02 19:15:36 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 2691
            Language: Python3
            Runtime: 20 ms
            Runtime percentile: 53.98
            Memory: 49.6 MB
            Memory percentile: 52.26

---
+++ 2691. Count Vowel Strings in Ranges/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        n = len(words)
+        Prefix = [0] * (n+1)
+        vowels = {"a", "e", "i", "o", "u"}
+
+        for i in range(n):
+            Prefix[i + 1] = Prefix[i]
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                Prefix[i + 1] +=1
+
+        ans = []
+        for q in queries:
+            ans.append(Prefix[q[1]+1] - Prefix[q[0]])
+        return ans
