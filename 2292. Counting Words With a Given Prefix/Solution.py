d30bb66e443828b7fbd49942c905a007ba05dd95
Mayowa Julius <69092079+Phenzic@users.noreply.github.com>
2025-01-09 21:01:35 +0100

    Python3 Solution
          Submission Statistics:
          Question #: 2292
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.892 MB
          Memory percentile: 20.89

---
+++ 2292. Counting Words With a Given Prefix/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        ans = 0
+        for i in words:
+            if i[:len(pref)] == pref:
+                ans += 1
+        return ans
+
+        
